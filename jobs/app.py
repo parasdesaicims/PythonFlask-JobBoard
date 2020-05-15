12879647144114a69e7529b530e8cd6ff52a696f
parasdesaicims <56562244+parasdesaicims@users.noreply.github.com>
2020-05-15 17:07:30 -0400

    4.5

--- jobs/app.py
+++ jobs/app.py
@@ -12,7 +12,7 @@ def open_connection():
     connection = getattr(g, '_connection', None)
     if connection == None:
         connection = g._connection = sqlite3.connect(PATH)
-    connection.row_factory = sqlite3.row
+    connection.row_factory = sqlite3.Row
     return connection
 
 
