b5d96bbbca92b4e551e3daa7798b0d8d29211449
parasdesaicims <56562244+parasdesaicims@users.noreply.github.com>
2020-05-15 09:47:22 -0400

    Update app.py

--- jobs/app.py
+++ jobs/app.py
@@ -3,6 +3,6 @@ from flask import Flask, render_template
 app=Flask(__name__)
 
 @app.route('/')
-@app.route('/jobs')
-def jobs():
+@app.route('/Jobs')
+def Jobs():
     return render_template('index.html')
